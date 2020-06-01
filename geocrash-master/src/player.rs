6accda183791114b6ca66c5cd0955be772f01000
dennisn00 <dxnnxs@protonmail.com>
2020-06-01 16:16:08 +0200

    added the creation of a RigidBody to player

--- geocrash-master/src/player.rs
+++ geocrash-master/src/player.rs
@@ -2,16 +2,47 @@ use ggez::graphics::{DrawParam, BlendMode, Mesh};
 use ggez::{graphics, Context, ContextBuilder, GameResult};
 use ggez::event::{self, EventHandler};
 use ggez::mint::Point2;
+use nphysics2d::object::{RigidBodyDesc, BodyStatus, RigidBody};
+use nalgebra::{Isometry2, Vector2};
+use rand::prelude::*;
+use std::f32::consts::PI;
+
 
 pub struct Player{
     //TODO: implement player attributes
     score: i32,
+    rigid_body: RigidBody<f64>,
 }
 
 impl Player {
     pub fn new() -> Self {
+
+        let left_bound = -50.0;
+        let right_bound = 50.0;
+        let top_bounds = 0.0;
+        let bottom_bounds = 50.0;
+
+        //should players get an id?? Could be helpful
+        let id = 1791;
+
+        let mut rng = rand::thread_rng();
+        let x_pos = rng.gen_range(left_bound, right_bound);
+        let y_pos = rng.gen_range(top_bounds, bottom_bounds);
+
+        let rigid_body= RigidBodyDesc::new()
+            .rotation(5.0)
+            .position(Isometry2::new(Vector2::new(x_pos, y_pos), PI))
+            .gravity_enabled(false)
+            .status(BodyStatus::Kinematic)
+            .max_linear_velocity(10.0)
+            .mass(5.0)
+            .build();
+
         //TODO: create a new player in the center of the screen
-        Player {}
+        Player {
+            score: 0,
+            rigid_body: rigid_body,
+        }
     }
 
     pub fn update() {
