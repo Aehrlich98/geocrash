231be9268a17db3c55732669b079be3ba95cfa58
Aehrlich98 <65243027+Aehrlich98@users.noreply.github.com>
2020-06-13 17:24:43 +0200

    Update master.rs

--- geocrash-master/src/master.rs
+++ geocrash-master/src/master.rs
@@ -26,7 +26,7 @@ pub const PLAYER_ID: i8 = 1;
 pub const GAME_OBJECT_ID: i8 = 2;
 
 pub struct Master{
-     window_mode: WindowMode,                            //holds data about window size
+    window_mode: WindowMode,                            //holds data about window size
     
     mechanical_world: DefaultMechanicalWorld<f32>,    //N/M types are somehow not right??? Maybe give specific types???
     geometrical_world: DefaultGeometricalWorld<f32>,
@@ -44,7 +44,6 @@ pub struct Master{
 
 impl Master{
     pub fn new(ctx: &mut Context, window_mode: WindowMode) -> Self{
-        window_mode,
         
         let mut force_generators = DefaultForceGeneratorSet::new();
 
@@ -58,6 +57,8 @@ impl Master{
         player.create_collider(&mut colliders);
 
         let mut master = Master{
+            window_mode,
+             
             mechanical_world: DefaultMechanicalWorld::new(Vector2::new(0.0, -9.81)),
             geometrical_world: DefaultGeometricalWorld::new(),
             bodies: bodies,
