e788d5d7df8d2b159ee6a7a5a77bb2f3e1bb164a
Aehrlich98 <65243027+Aehrlich98@users.noreply.github.com>
2020-06-16 01:37:30 +0200

    Added function to create boundaries for game world
    create_bound generates a rigidbody and collider as a cuboid at given position and inserts them into the Body- ColliderSet of master.
    It returns a tupel of the rigid body and collider handle.
    
    The method is called four times in main to create a cuboid per edge

--- geocrash-master/src/main.rs
+++ geocrash-master/src/main.rs
@@ -35,6 +35,12 @@ fn main() {
     let mut my_game = Master::new(&mut ctx);
 
     //spawn starting objects:
+    //create boundaries for game world: (left, right, upper, lower)
+    create_bound(&mut my_game, Vector2::new(0.0, window_mode.height/2.0), Vector2::new(0.1, window_mode.height));
+    create_bound(&mut my_game, Vector2::new(window_mode.width, window_mode.height/2.0), Vector2::new(0.1, window_mode.height));
+    create_bound(&mut my_game, Vector2::new(window_mode.width/2.0, 0.0), Vector2::new(window_mode.width, 0.1));
+    create_bound(&mut my_game, Vector2::new(window_mode.width/2.0, window_mode.height), Vector2::new(window_mode.width, 0.1));
+    
     //TODO spawn player(s)
     //spawn other GameObjects
     let pos = (0, 0);
@@ -60,4 +66,29 @@ fn main() {
         Ok(_) => println!("Exited cleanly."),
         Err(e) => println!("Error occured: {}", e)
     }
+}
+
+
+//create a boundary for the game world at pos position. The boundary is a cuboid with rigid body and collider
+//with the dimensions of size: Vector2 at position pos: Vector2
+//the rbs and colliders are then added into the BodySet and ColliderSet of master
+fn create_bound(master: &mut Master, pos: Vector2<f32>, size: Vector2<f32>) -> (DefaultBodyHandle, DefaultColliderHandle) {
+    let bound_rb = RigidBodyDesc::new()
+        .position(Isometry2::new(pos, 0.0))
+        .gravity_enabled(false)
+        .status(BodyStatus::Kinematic)                      //make rigid body immovable
+        .kinematic_translations(Vector2::new(false, false))
+        .kinematic_rotations(false)
+        .build();
+    let bound_rb_handle = master.bodies.insert(bound_rb);
+    let bound_shape = ShapeHandle::new(Cuboid::new(size));
+    let collider = ColliderDesc::new(bound_shape)
+        .density(1.0)
+        .material(MaterialHandle::new(BasicMaterial::new(0.4, 0.6)))    //TODO (?) fine tune values
+        .margin(0.1f32)
+        .user_data(master::BOUND_ID)
+        .build(BodyPartHandle(bound_rb_handle, 0));
+    let collider_handle = master.colliders.insert(collider);
+
+    return (bound_rb_handle, collider_handle);
 }
