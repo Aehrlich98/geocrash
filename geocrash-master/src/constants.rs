43736b98ff5358dc983a9dac2f6a1c9a6d61e01c
dennisn00 <dxnnxs@protonmail.com>
2020-06-23 10:48:00 +0200

    exported constants to file

---
+++ geocrash-master/src/constants.rs
@@ -0,0 +1,9 @@
+
+pub const LEFT: i8 = 0;
+pub const RIGHT: i8 = 1;
+pub const UP: i8 = 2;
+pub const DOWN: i8 = 3;
+pub const PLAYER_ID: i8 = 4;
+pub const GAME_OBJECT_ID: i8 = 5;
+pub const BOUND_ID: i8 = 6;
+pub const GAME_SIZE: i32 = 600;
